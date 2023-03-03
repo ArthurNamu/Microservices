579d839db827c40c6856dd7db6775e02f7690518
Arthur Namu <ArthurN@hollywoodbets.net>
2023-03-03 22:54:16 +0300

    Added DI for Repository

--- PlatformService/PlatformService/Program.cs
+++ PlatformService/PlatformService/Program.cs
@@ -9,6 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
 builder.Services.AddDbContext<AppDBContext>(opt =>
 opt.UseInMemoryDatabase("InMem"));
 

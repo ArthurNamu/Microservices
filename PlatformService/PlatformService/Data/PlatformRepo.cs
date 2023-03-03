65e09b9ca06ec4350a816b03c54c984b2951b087
Arthur Namu <ArthurN@hollywoodbets.net>
2023-03-03 22:48:15 +0300

    Created new Platform microservice | Added EF Repo

---
+++ PlatformService/PlatformService/Data/PlatformRepo.cs
@@ -0,0 +1,32 @@
+﻿using PlatformService.Models;
+
+namespace PlatformService.Data;
+
+public class PlatformRepo : IPlatformRepo
+{
+    private readonly AppDBContext _context;
+
+    public PlatformRepo(AppDBContext context)
+    {
+        _context = context;
+    }
+    public void CreatePlatform()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Platform GetPlatformById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Platform> GetAllPlatforms()
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool SaveChanges()
+    {
+        return (_context.SaveChanges() >= 0);
+    }
+}

d88d90dc8cff5dda843267e1eb35b070b5d7ca04
Arthur Namu <ArthurN@hollywoodbets.net>
2023-03-05 16:41:58 +0300

    Added Comand Service

---
+++ PlatformService/CommandService/Controllers/PlatformsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CommandService.Controllers;
+[Route("api/c/[Controller]")]
+[ApiController]
+public class PlatformsController : ControllerBase
+{
+	public PlatformsController()
+	{
+
+	}
+
+	[HttpPost]
+	public ActionResult TestInboundConnection()
+	{
+		Console.WriteLine("--> Inbound POST # command Service");
+
+		return Ok("--> Inbound test from Platforms Controller");
+	}
+}

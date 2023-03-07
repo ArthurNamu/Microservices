34d0dd4bd27b88ce8e365412377c55f96289d641
Arthur Namu <ArthurN@hollywoodbets.net>
2023-03-07 18:54:14 +0300

    Added Automapper profiles

---
+++ CommandService/CommandService/Profiles/CommandsProfiles.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using CommandService.Dtos;
+using CommandService.Models;
+
+namespace CommandService.Profiles;
+public class CommandsProfiles : Profile
+{
+	public CommandsProfiles()
+	{
+		//source -> target
+		CreateMap<Platform, PlatformReadDto>();
+		CreateMap<CommandCreateDto, Command>();
+		CreateMap<Command, CommandReadDto>();
+
+	}
+}
